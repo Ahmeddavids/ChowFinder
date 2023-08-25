721f45b92d3dd241394aca1a716cb44699e01f5d
Ahmeddavids <ahmeddavids6@gmail.com>
2023-08-25 04:40:38 +0100

    Set back to the initial state

--- server.js
+++ server.js
@@ -18,14 +18,7 @@ app.use(express.json());
 app.use(fileUpload({
     useTempFiles: true
 }));
-// app.use(cors({origin: "*"}));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
-app.use(cors({origin: "http://127.0.0.1:5173"}));
-
+app.use(cors({origin: "*"}));
 
 
 app.use('/api', userRouter);
